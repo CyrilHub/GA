f48e8dcbd17b4cf7e26558c322f48d7104a4cf15
Cyril <cyril.p.blank@gmail.com>
2019-01-03 19:23:59 -0500

    Initial Commit

---
+++ Ach/Odd_Number.py
@@ -0,0 +1,10 @@
+number = 10
+remainder = number % 2
+
+if remainder == 0:
+  print("O")
+elif remainder == 1:
+    print ("1")
+
+else:
+  print("It's odd!")
---
+++ Ach/movie_title.py
@@ -0,0 +1,3 @@
+movie_title = "Back to the Future"
+movie_ratings = "8"
+print("The rating for "+ movie_title+" is "+movie_ratings)
